5f608a8c80f80565c727cf74dc5add505eaccbad
bRAAz <jvtomedi@gmail.com>
2024-04-25 00:13:15 -0300

    Criação do cadastrar setor e ajuste na porta 5432 para 5433

--- api/src/main/java/com/ong/api/controller/setorController.java
+++ api/src/main/java/com/ong/api/controller/setorController.java
@@ -3,9 +3,7 @@ package com.ong.api.controller;
 import com.ong.api.entity.Setor;
 import com.ong.api.repository.SetorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -15,8 +13,13 @@ public class setorController {
     @Autowired
     SetorRepository repository;
 
+
     @GetMapping
     public List<Setor> setorList(){
         return repository.findAll();
+    }
+    @PostMapping
+    public void setorCreate(@RequestBody Setor setor){
+        repository.save(setor);
     }
 }
--- api/src/main/java/com/ong/api/entity/Setor.java
+++ api/src/main/java/com/ong/api/entity/Setor.java
@@ -11,4 +11,30 @@ public class Setor {
     @OneToOne
     @JoinColumn(name = "usuario_id")
     private Usuario leader;
+
+    public Setor(String descricao){
+        this.descricao = descricao;
+    }
+    public Setor(String descricao, Usuario leader){
+        this.descricao = descricao;
+        this.leader = leader;
+    }
+    public Long getId(){
+        return id;
+    }
+    public void setId(Long id){
+        this.id = id;
+    }
+    public String getDescricao(){
+        return descricao;
+    }
+    public void setDescricao(String descricao){
+        this.descricao = descricao;
+    }
+    public Usuario getLeader(){
+        return leader;
+    }
+    public void setLeader(Usuario leader){
+        this.leader = leader;
+    }
 }
